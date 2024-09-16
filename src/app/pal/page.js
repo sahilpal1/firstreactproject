d8e3f7fb760e74d3be36ee635086d8dbde116b03
sahilpal1 <sahilpal06072004@gmail.com>
2024-09-16 12:23:02 +0530

    first commit

---
+++ src/app/pal/page.js
@@ -0,0 +1,12 @@
+import styles from "./page.module.css"
+export default function About() {
+    return (
+      <div>
+        <h1 className={styles.heading}> sahil pal About page </h1>
+      </div>
+      
+          
+  
+    );
+  }
+  
